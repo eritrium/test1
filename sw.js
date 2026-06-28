4fdd1f4c82f78ad3328e301d64818ffb5a56a845
eritrium <eritriumakira@gmail.com>
2026-06-28 17:36:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-gestionale-v2'; // Aggiornato a v2 per forzare il reset
+const CACHE_NAME = 'taxi-gestionale-v3'; // Aggiornato a v2 per forzare il reset
 const ASSETS = [
   './',
   './index.html',
